dd8a7d6eb003edeabff0f409176b909c5502789d
AlexisDevjs <alexis.devct@gmail.com>
2026-09-25 10:32:53 -0500

    fix: configurar el adaptador de Vercel con soporte para servicio de imágenes y optimización de caché

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,14 @@ import vercel from '@astrojs/vercel'
 export default defineConfig({
   site: 'https://www.mmslcontracting.com',
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    imageService: true,
+    imagesConfig: {
+      sizes: [150, 300, 480, 600, 640, 750, 828, 1080, 1200, 1600, 1920, 2048, 3840],
+      formats: ['image/webp'],
+      minimumCacheTTL: 2678400,
+    },
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
